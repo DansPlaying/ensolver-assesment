90ff667b1e86459853173502d24d712863dff693
Daniel Alejandro Urbina <daniel.alejandro.07@hotmail.com>
2026-03-28 18:08:37 -0400

    Add component barrel exports

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,6 @@
+export { Header } from './Header';
+export { Modal } from './Modal';
+export { NoteCard } from './NoteCard';
+export { NoteForm } from './NoteForm';
+export { CategoryBadge } from './CategoryBadge';
+export { CategoryFilter } from './CategoryFilter';
