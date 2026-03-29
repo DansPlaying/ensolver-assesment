92350d237996e6838f5918c3794576abd347cbe1
Daniel Alejandro Urbina <daniel.alejandro.07@hotmail.com>
2026-03-28 21:10:35 -0400

    Add notes module

---
+++ backend/src/notes/notes.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotesController } from './notes.controller';
+import { NotesService } from './notes.service';
+import { Note } from '../entities/note.entity';
+import { Category } from '../entities/category.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Note, Category])],
+  controllers: [NotesController],
+  providers: [NotesService],
+})
+export class NotesModule {}
